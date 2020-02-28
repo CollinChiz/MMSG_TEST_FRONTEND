224e6af4d2334ff55f0dc5163fec26060d556afa
chizzy <cwchiz16@gmail.com>
2020-02-27 17:47:45 -0700

    add results to the page

---
+++ index.js
@@ -0,0 +1,26 @@
+
+    $("form").on("submit", function(e) {
+        e.preventDefault();
+        var currency = $(".currency").val().trim();
+        var amount = $(".amount").val().trim();
+        var targetCurrency = $(".target-currency").val().trim();
+    
+        console.log(currency, amount, targetCurrency);
+        
+        var queryURL = `https://mmsgtest.herokuapp.com/?currency=${currency}&base_amount=${amount}&target_currency=${targetCurrency}`;
+
+        $.ajax({
+            url: queryURL,
+            method: "POST"
+          })
+          .then(function(res) {
+              console.log(res.currency);
+
+              var results = Math.round(res.amount * 100) / 100;
+              console.log(results);
+
+              $(".results").html(`<h1>${res.currency}</h1> <h3>${results}</h3>`)
+              
+
+          })
+    })
